25fafa05128b2e548a5352de9d06db1cf247f862
Marsch Huynh <marsch.huynh@gmail.com>
2017-04-07 20:11:06 +0700

    Init project and takePicture function

---
+++ app/src/main/java/com/bkai/flowerdetect_android/MainActivity.java
@@ -0,0 +1,175 @@
+package com.bkai.flowerdetect_android;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.Surface;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    static {
+        if(!OpenCVLoader.initDebug())
+        {
+            Log.e("OpenCv","Init Fail");
+        } else {
+            Log.e("OpenCv","Init Successful");
+        }
+    }
+
+    Toolbar toolbar;
+    private static final String TAG = "OCVSample::Activity";
+    private CameraView mOpenCvCameraView;
+    private boolean mIsJavaCamera = true;
+    private MenuItem mItemSwitchCamera = null;
+    Mat mRgba;
+    Mat mRgbaF;
+    Mat mRgbaT;
+
+    Button takePicure;
+
+
+    private BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG,"OpenCV load successfully");
+                    mOpenCvCameraView.enableView();
+
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    private void checkPermission(){
+        int hasWriteContactsPermission = ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA);
+        if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    123);
+            Log.e("Permission","Camera");
+            return;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkPermission();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        mOpenCvCameraView = (CameraView) findViewById(R.id.CameraView);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+        takePicure = (Button) findViewById(R.id.takePicture);
+        takePicure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+                String currentDateandTime = sdf.format(new Date());
+                String sdcard = Environment.getExternalStorageDirectory().getPath();
+                File path = new File(sdcard+"/FlowerDetect/");
+                path.mkdirs();
+                String fullPath = sdcard + "/FlowerDetect/" + currentDateandTime + ".jpg";
+                mOpenCvCameraView.takePicture(fullPath, null);
+            }
+        });
+    }
+
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaT = new Mat(height, width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        mRgba = inputFrame.rgba();
+        switch (mOpenCvCameraView.getDisplay().getRotation()) {
+            case Surface.ROTATION_0: // Vertical portrait
+                Core.transpose(mRgba, mRgbaT);
+                Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(), 0,0, 0);
+                Core.flip(mRgbaF, mRgba, 2);
+                break;
+            case Surface.ROTATION_90: // 90° anti-clockwise
+                break;
+            case Surface.ROTATION_180: // Vertical anti-portrait
+                Core.transpose(mRgba, mRgbaT);
+                Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(), 0,0, 0);
+                Core.flip(mRgbaF, mRgba, 0);
+                break;
+            case Surface.ROTATION_270: // 90° clockwise
+                Imgproc.resize(mRgba, mRgbaF, mRgbaF.size(), 0,0, 0);
+                Core.flip(mRgbaF, mRgba, -1);
+                break;
+            default:
+        }
+        return mRgba;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!OpenCVLoader.initDebug()){
+            Log.e(TAG, "Internal OpenCv Library not found.");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallBack);
+        } else {
+            Log.e(TAG, "OpenCv library found inside package.");
+            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mOpenCvCameraView != null){
+            mOpenCvCameraView.disableView();
+        }
+    }
+}
